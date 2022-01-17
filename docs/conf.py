b6e669fd4157869278c394333724f16ea41f30f5
Sanskar Jain <sanskarjain1997.19@gmail.com>
2022-01-17 13:59:30 +0530

    edited conf.py to get version number of the package

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import medium_apis
+# import medium_apis
 import pkg_resources
 
 # -- General configuration ---------------------------------------------
